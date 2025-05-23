110248c11ec36b47856a8a60c3029d79d3b155c0
Otido Peter <peterotido@gmail.com>
2025-05-23 22:14:24 +0300

    if statements

--- bank.go
+++ bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance float64 = 1000.0
+
 	fmt.Print("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
@@ -11,7 +13,14 @@ func main() {
 	fmt.Println("4. Exit")
 
 	var choice int
+	fmt.Print("Your choice: ")
 	fmt.Scan(&choice)
 
-	fmt.Println("Your choice: ", choice)
+	// wantsCheckBalance := choice == 1
+
+	if choice == 1 {
+		fmt.Println("Your balance is", accountBalance)
+	}
+
+	fmt.Println("Your choice:", choice)
 }
