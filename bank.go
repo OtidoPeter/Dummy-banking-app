139ed25db294699459112c1de4c54dc699ed7bc7
Otido Peter <peterotido@gmail.com>
2025-06-10 06:24:37 +0300

    splitting code into files

--- bank.go
+++ bank.go
@@ -134,10 +134,3 @@ func main() {
 
 	}
 }
-func presentOptions() {
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
-}
