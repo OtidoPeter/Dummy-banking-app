cd5e84708286011e6f785b43e1f8cccb64d9e043
Otido Peter <peterotido@gmail.com>
2025-07-03 06:52:10 +0300

    getting to choose from options

--- bank.go
+++ bank.go
@@ -9,4 +9,10 @@ func main() {
 	fmt.Println("2. Deposit money")
 	fmt.Println("3. Withdraw money")
 	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	fmt.Println("Your choice:", choice)
 }
