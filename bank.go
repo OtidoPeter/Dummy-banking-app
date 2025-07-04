2a1eee871caab6caff331a579648504ec2c23e3a
Otido Peter <peterotido@gmail.com>
2025-07-04 06:52:00 +0300

    incorporating else if

--- bank.go
+++ bank.go
@@ -7,7 +7,7 @@ func main() {
 
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance?")
+	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposit money")
 	fmt.Println("3. Withdraw money")
 	fmt.Println("4. Exit")
@@ -20,6 +20,13 @@ func main() {
 
 	if choice == 1 {
 		fmt.Println("Your balance is", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+
+		fmt.Println("Balance updated! New amount:", accountBalance)
 	}
 
 	fmt.Println("Your choice:", choice)
