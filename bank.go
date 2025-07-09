2273c92205412bcecab5afdf34a56cc7fd6b543a
Otido Peter <peterotido@gmail.com>
2025-07-09 06:50:01 +0300

    getting out of the loop

--- bank.go
+++ bank.go
@@ -54,8 +54,9 @@ func main() {
 			fmt.Println("Balance updated! New amount:", accountBalance)
 		} else {
 			fmt.Println("Goodbye!")
-			return
+			//return
+			break
 		}
 	}
-
+	fmt.Println("Thanks for choosing our bank")
 }
